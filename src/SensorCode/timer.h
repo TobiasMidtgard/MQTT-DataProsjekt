bbbb205fe5d38e1ac18a07c34c4dae5d3f0e551f
adam-gm <80262051+adam-gm@users.noreply.github.com>
2022-11-24 16:58:28 +0100

    Update timer.h
    Endret på oppsettet slik at det ikke defineres medlemfunksjon for hver timer

--- src/SensorCode/timer.h
+++ src/SensorCode/timer.h
@@ -13,48 +13,29 @@ public:
    *
    * @param timeout starts a timer for how often measurments should be made.
    * @return
-   */
-  void bmeStart(int timeout);
-
-  /**
+   *
+   *
    * @brief Starts a timer that controls how often data should be published to main server.
    *
    * @param timeout starts a timer for how often data should be published.
    * @return
-   */
-  void publishStart(int timeout);
-
-  /**
+   *
+   *
    * @brief A timer after the alarm that cotrols when the microcontroller can send data.
    *
    * @param timeout starts a timer for how often data should be published.
    * @return
    */
-  void alarmStart(int timeout);
+  void start(int timer);
 
   /**
-   * @brief Checks if the timer has run out for the BME sensor.
+   * @brief Checks if the timer has run out.
    *
    * @return If the timer has expired.
    */
-  bool bmeHasExpired();
+  bool hasExpired();
 
-  /**
-   * @brief Checks if the publish timer has run out.
-   *
-   * @return If the timer has expired.
-   */
-  bool publishHasExpired();
-
-  /**
-   * @brief Checks if the alarm timer has run out.
-   *
-   * @return If the timer has expired.
-   */
-  bool alarmTimerExpired();
 
 private:
-  unsigned long SbmeTimeout;
-  unsigned long publishTimeout;
-  unsigned long alarmTimeout;
+  unsigned long timeout;
 };

a8f6e448dc7387da0b1a7eb5c4b9351facc4f607
adam-gm <80262051+adam-gm@users.noreply.github.com>
2022-11-26 13:39:14 +0100

    Update timer.h
    Fjernet noen kommentarer

--- src/SensorCode/timer.h
+++ src/SensorCode/timer.h
@@ -9,23 +9,12 @@ public:
   // Methods:
 
   /**
-   * @brief Starts a timer for the BME sensor for how often the sensor should take measurements.
+   * @brief Starts a timer.
    *
-   * @param timeout starts a timer for how often measurments should be made.
-   * @return
-   *
-   *
-   * @brief Starts a timer that controls how often data should be published to main server.
-   *
-   * @param timeout starts a timer for how often data should be published.
-   * @return
-   *
-   *
-   * @brief A timer after the alarm that cotrols when the microcontroller can send data.
-   *
-   * @param timeout starts a timer for how often data should be published.
+   * @param timeout starts a timer for how often measurments or publishment should be made.
    * @return
    */
+  
   void start(int timer);
 
   /**
